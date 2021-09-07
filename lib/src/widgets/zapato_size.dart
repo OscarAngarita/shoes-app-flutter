8c283148717e6279f0d416fa60dc3b99c0788b93
OscarAngarita <oscarangarita05@gmail.com>
2021-09-06 23:30:49 -0500

    Updated: main shoe design finished

--- lib/src/widgets/zapato_size.dart
+++ lib/src/widgets/zapato_size.dart
@@ -20,7 +20,7 @@ class ZapatoSizePreview extends StatelessWidget {
             _ZapatoConSombra(),
 
             //Tallas
-
+            _ZapatoTallas()
           ],
         ),
         
@@ -30,7 +30,65 @@ class ZapatoSizePreview extends StatelessWidget {
 }
 
 
+//Tallas
+class _ZapatoTallas extends StatelessWidget {
 
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _CajaTalla(7),
+          _CajaTalla(7.5),
+          _CajaTalla(8),
+          _CajaTalla(8.5),
+          _CajaTalla(9),
+          _CajaTalla(9.5),
+        ],
+      ),
+    );
+  }
+}
+
+class _CajaTalla extends StatelessWidget {
+
+  final double talla;
+  
+  const _CajaTalla(
+   this.talla,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      width: 45,
+      height: 45,
+      decoration: BoxDecoration(
+        color: (this.talla==9) ? Color(0xffF1A23A) : Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          if(this.talla==9)
+            BoxShadow(color: Color(0xffF1A23A), blurRadius: 10, offset: Offset(0,5))
+        ]
+      ),
+
+      child: Text(
+        '${talla.toString().replaceAll('.0', '')}', 
+        style: TextStyle(
+          color: (this.talla==9) ? Colors.white : Color(0xffF1A23A),
+          fontSize: 16,
+          fontWeight: FontWeight.bold
+        ),
+      ),
+    );
+  }
+}
+
+
+//Zapato&Sombra
 class _ZapatoConSombra extends StatelessWidget {
 
   @override
@@ -40,10 +98,34 @@ class _ZapatoConSombra extends StatelessWidget {
       child: Stack(
         children: [
 
-          Image(image: AssetImage('assets/imgs/azul.png')),
+          Positioned(
+            bottom: 20,
+            right: 0,
+            child: _Sombra()
+          ),
 
-          
+          Image(image: AssetImage('assets/imgs/azul.png')), 
         ],
+      ),
+    );
+  }
+}
+
+class _Sombra extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: -0.5,
+      child: Container(
+        width: 230,
+        height: 120,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          boxShadow: [
+            BoxShadow(color: Color(0xffEAA14E), blurRadius: 40)
+          ]
+        ),
+        
       ),
     );
   }
